924ddde7662d2a7c9776f0f362858fe53a1aebdb
사재원 <aion97@naver.com>
2024-10-27 12:56:17 +0900

    pagenotfound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,26 @@
+import styled from "styled-components";
+
+const Notice = styled.div`
+font-size: 60px;
+text-align:center;
+h1{
+  font-size:80px;
+  margin-bottom:30px;
+}
+p{
+  margin-top: 30px;
+  font-size:30px;
+}
+`;
+
+
 const PageNotFound = () => {
   return <div>
-    <h2>반갑다</h2>
+    <Notice>
+    <h1>404</h1>
+      Oops, This Page Not Found!
+    <p>페이지 경로를 잘못 입력하신듯 합니다. <br/> 홈페이지로 돌아가려면 로고를 클릭해주세요</p>
+      </Notice>
   </div>;
 };
 
